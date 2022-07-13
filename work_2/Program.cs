a23df99c79cb6c0dd5f3046ad24d17ec4bd3984c
1 <1@LAPTOP-ILGMJ5ES>
2022-07-13 03:05:30 +0300

    Добавьте файлы проекта.

---
+++ work_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        double firstValue, secondValue, thirdValue;
+
+        Console.WriteLine("Введите 1 число");
+        firstValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите 2 число");
+        secondValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите 3 число");
+        thirdValue = double.Parse(Console.ReadLine());
+
+        double sumResult = firstValue + secondValue + thirdValue;
+        Console.WriteLine(sumResult);
+        double multResult = firstValue * secondValue * thirdValue;
+        Console.WriteLine(multResult);
+
+    }
+}
